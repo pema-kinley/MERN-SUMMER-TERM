ac4e292597347510df623a96f4db9e5de9fceba3
Pema Kinley <pemakinley1333@gmail.com>
2026-07-01 04:31:24 +0530

    added hero section

--- key-shop/src/pages/Home.jsx
+++ key-shop/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import Navbar from "../components/Navbar";
+import Hero from "../sections/home/Hero/Hero";
+import FeaturedCreations from "../sections/home/FeaturedCreations";
+import HowItWorks from "../sections/home/HowItWorks";
+import WhyChooseUs from "../sections/home/WhyChooseUs";
+import CustomerStories from "../sections/home/CustomerStories";
 
 function Home() {
-    return <h1>Home Page</h1>;
+    return (
+        <main>
+            <Hero />
+            <FeaturedCreations />
+            <HowItWorks />
+            <WhyChooseUs />
+            <CustomerStories />
+        </main>
+    );
 }
 
 export default Home;
