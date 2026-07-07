05daea8d54a4f7d9af6be0c17f96093a1bf34c3d
Pema Kinley <pemakinley1333@gmail.com>
2026-07-08 01:32:50 +0530

    styled the Navbar

--- key-shop/src/components/Navbar.jsx
+++ key-shop/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
     return (
-        <nav>
-            <h1>DIY Key-Chain Shop</h1>
+        <nav className={styles.navbar}>
+            <h1 className={styles.logo}>Echo & Ember</h1>
 
-            <Link to = "/">Home</Link>
-            <Link to = "/shop">Shop</Link>
+            <div className={styles.navLinks}>
+                <Link to = "/">Home</Link>
+                <Link to = "/shop">Shop</Link>
+            </div>
         </nav>
     );
 }
