61974cc851299913ce07241e475f1800b5d94342
Pema Kinley <pemakinley1333@gmail.com>
2026-07-11 05:26:20 +0530

    object destructuring for props

--- key-shop/src/components/ProductCard/ProductCard.jsx
+++ key-shop/src/components/ProductCard/ProductCard.jsx
@@ -1,24 +1,39 @@
 import styles from "./ProductCard.module.css";
 
-function ProductCard(props) {
+function ProductCard({
+    title,
+    image,
+    category,
+    price,
+    rating,
+    reviewCount,
+    isPersonalized,
+}) {
     return (
         <article className={styles.card}>
             <div className={styles.image}>
-                <img 
-                    src={props.image}
-                    alt={props.title}
-                />
+                <img src={image} alt={title} />
+                {isPersonalized && (
+                    <span className={styles.badge}>✨ Personalized</span>
+                )}
             </div>
+
             <div className={styles.content}>
-                <h3>{props.title}</h3>
-                <p>{props.price}</p>
+                <div className={styles.topInfo}>
+                    <p className={styles.category}>{category}</p>
+                    <h3 className={styles.title}>{title}</h3>
+                    <p className={styles.rating}>
+                        ⭐ {rating} <span className={styles.reviewCount}>({reviewCount} Reviews)</span>
+                    </p>
+                </div>
 
-                <button className={styles.addToCartButton}>
-                    Add to Cart
-                </button>
+                <div className={styles.bottomInfo}>
+                    <p className={styles.price}>Nu. {price}</p>
+                    <button className={styles.addToCartButton}>
+                        Add to Cart
+                    </button>
+                </div>
             </div>
-
-            
         </article>
     );
 }
